5127e7d3dac9fc1cc94a1fb3708a6e111b3fc7a9
Dave Novak <david.novak@thoughtbot.com>
2022-05-03 13:19:10 -0700

    configure some netlify thingies

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ import adapter from '@sveltejs/adapter-netlify'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		target: '#svelte',
 		adapter: adapter()
 	}
 };
+
+export default config;
